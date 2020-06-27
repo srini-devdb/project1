deea3eec3424aee4f1757f640866da38f207071f
srini-devdb <srini.devdb@gmail.com>
2020-06-27 09:08:08 -0400

    updated

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_xqgpIAKvW', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4sdrgmenug831bv3u8v1l3k8af', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Pzg68s1Xj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'c259bg658ji62onbv7b1osdeu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
